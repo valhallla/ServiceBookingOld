95ce1f1d6aad81db82de2ad597130732c2b8d20b
valhallla <kruner.kruner@gmail.com>
2017-03-06 15:47:03 +0300

    Add project files.

---
+++ ServiceBooking/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ServiceBooking.MvcApplication" Language="C#" %>
---
+++ ServiceBooking/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ServiceBooking.Startup))]
+namespace ServiceBooking
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
